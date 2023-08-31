3cd81535e71a46325f4286689eb3f2048ed33cfc
boddicheg <boddicheg@gmail.com>
2023-09-01 00:36:21 +0300

    Update for geolocation class

--- notebook/deps/GeoLocation.py
+++ notebook/deps/GeoLocation.py
@@ -1,12 +1,9 @@
 import geoip2.database
 import socket, struct, os
 
-DB_NAME = "assets/geo/dbip-city-lite-2023-02.mmdb"
-
 class GeoLocation():
-    def __init__(self, ROOT):
-        db = os.path.join(ROOT, DB_NAME)
-        self.reader = geoip2.database.Reader(db)
+    def __init__(self, DB_NAME):
+        self.reader = geoip2.database.Reader(DB_NAME)
 
     def ip2long(self, ip):
         packed = socket.inet_aton(ip)
