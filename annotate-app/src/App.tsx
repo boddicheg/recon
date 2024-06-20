b9131c81ce5817611c2204e7a25f7ab6d89e25d4
boddicheg <boddicheg@gmail.com>
2024-06-20 17:09:15 +0300

    Add simple routes: Projects and Dashboard

--- annotate-app/src/App.tsx
+++ annotate-app/src/App.tsx
@@ -1,20 +1,27 @@
-import './index.css';
-import HeaderComponent from './components/HeaderComponent';
+import "./index.css";
+import Header from "./components/HeaderComponent";
+import Dashboard from "./components/DashboardComponent";
+import Projects from "./components/ProjectsComponent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <div className="min-h-full">
-        <HeaderComponent />
+      <Router>
+        <div className="min-h-full">
+          <Header />
           <main>
             <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+              </Routes>
             </div>
-        </main>
-      </div>
+          </main>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- annotate-app/src/components/HeaderComponent.tsx
+++ annotate-app/src/components/HeaderComponent.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
 
-const HeaderComponent: React.FC = () => {
+const Header: React.FC = () => {
   return (
     <>
-        <nav className="bg-gray-800">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <div className="flex h-16 items-center justify-between">
-                  <div className="flex items-center">
-                      <div className="flex-shrink-0">
-                          <img className="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/></div>
-                      <div className="hidden md:block">
-                          <div className="ml-10 flex items-baseline space-x-4">
-                              <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Projects</a>
-                          </div>
-                      </div>
-                  </div>
+      <nav className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8 w-8"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
               </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                    <Link to="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Dashboard</Link>
+                    <Link to="/projects" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Projects</Link>
+                </div>
+              </div>
+            </div>
           </div>
-      </nav>
-      <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Projects</h1>
         </div>
-    </header></>
+      </nav>
+    </>
   );
 };
 
-export default HeaderComponent;
+export default Header;
