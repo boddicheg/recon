a13e97cd66f32fb3faad0d362674becc0fc24c6b
boddicheg <bbobyr@logitech.com>
2024-07-23 15:30:38 +0300

    Compilation fixes

--- frontend/src/services/Api.tsx
+++ frontend/src/services/Api.tsx
@@ -14,7 +14,7 @@ export const fetchProjects = async (): Promise<Array<ProjectsInterface>> => {
   return await response.json();
 };
 
-interface NewProjectData {
+export interface NewProjectData {
   name: string;
   description: string;
 }
