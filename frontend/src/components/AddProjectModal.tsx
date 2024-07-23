d837505ced52f57c40e7e702836815be1a2895a9
boddicheg <boddicheg@gmail.com>
2024-07-23 16:34:23 +0300

    tsx error fixes

--- frontend/src/components/AddProjectModal.tsx
+++ frontend/src/components/AddProjectModal.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
 
-import { sendAddNewProject, NewProjectData } from "../services/Api";
+import { sendAddNewProject } from "../services/Api";
 
 interface AddProjectModalProps {
   onAddProject: (msg: string) => void;
@@ -9,11 +9,10 @@ interface AddProjectModalProps {
 
 const AddProjectModal: React.FC<AddProjectModalProps> = ({ onAddProject }) => {
   const [showModal, setShowModal] = useState(false);
-  const [formData, setFormData] = useState<NewProjectData>({ name: '', description: '' });
-  const [response, setResponse] = useState<string>('');
-  const [error, setError] = useState<string>('');
-  const [name, setName] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
+  const [response, setResponse] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -25,22 +24,15 @@ const AddProjectModal: React.FC<AddProjectModalProps> = ({ onAddProject }) => {
     setError("");
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setError('');
-    setResponse('');
+    setError("");
+    setResponse("");
 
     const data = { name, description };
 
     try {
-      const result = await sendAddNewProject(data);
-      // console.log(data)
-      // setResponse(result.message);
+      await sendAddNewProject(data);
       setShowModal(false);
       onAddProject("");
     } catch (err) {
@@ -62,58 +54,58 @@ const AddProjectModal: React.FC<AddProjectModalProps> = ({ onAddProject }) => {
         + Add new project
       </button>
       <Modal show={showModal} onClose={handleCloseModal}>
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    autoComplete="name"
-                    required
-                    onChange={(e) => setName(e.target.value)}
-                    className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                  className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
               </div>
-              <div>
-                <label
-                  htmlFor="description"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Description
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="description"
-                    name="description"
-                    type="text"
-                    autoComplete="description"
-                    required
-                    onChange={(e) => setDescription(e.target.value)}
-                    className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+            </div>
+            <div>
+              <label
+                htmlFor="description"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Description
+              </label>
+              <div className="mt-2">
+                <input
+                  id="description"
+                  name="description"
+                  type="text"
+                  autoComplete="description"
+                  required
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
               </div>
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 py-1.5 px-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Add
-                </button>
-              </div>
-            </form>
-            {response && <p>Response: {response}</p>}
-            {error && <p>Error: {error}</p>}
-          </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 py-1.5 px-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Add
+              </button>
+            </div>
+          </form>
+          {response && <p>Response: {response}</p>}
+          {error && <p>Error: {error}</p>}
+        </div>
       </Modal>
     </div>
   );
